1d3bf6f61b4a285258fc69d44367876947b787f8
Marifel Barbasa <mrbarbasa@gmail.com>
2016-05-25 21:07:46 -1000

    Create an express server and use webpack-dev-middleware with webpack-hot-middleware to enable HMR; remove webpack-dev-server and react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const root = path.resolve(__dirname, 'src');
 
 module.exports = {
+  devtool: 'source-map',
   entry: [
+    'webpack-hot-middleware/client',
     path.resolve(__dirname, 'src/index.js')
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
@@ -21,14 +22,18 @@ module.exports = {
     }),
     new webpack.ProvidePlugin({
       'React': 'react'
-    })
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-        include: root,
-        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
+        exclude: /node_modules/,
+        include: root
       }
     ]
   }
